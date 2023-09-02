a271370b74b3bc7169c25e777364b2afd0d2510c
matsurix28 <80552055+matsurix28@users.noreply.github.com>
2023-09-02 20:16:19 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 yes n | pkg update -y && pkg upgrade -y
 pkg install git
-git clone https://github.com/knterada5/termubuntu.git $HOME/termubuntu
+git clone https://github.com/matsurix28/termubuntu.git $HOME/termubuntu
 bash $HOME/termubuntu/scripts/start_termux.sh
