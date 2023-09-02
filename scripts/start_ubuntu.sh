70f84b66480867284f885962f9b39a13afbd19a0
matsurix28 <80552055+matsurix28@users.noreply.github.com>
2023-09-02 20:17:05 +0900

    Update start_ubuntu.sh

--- scripts/start_ubuntu.sh
+++ scripts/start_ubuntu.sh
@@ -11,7 +11,7 @@ sudo dpkg-reconfigure --frontend noninteractive tzdata
 # Download dotfiles.
 read -n1 -p "Do you clone dotfiles? It will take a long time to set up. [y/N]: " yn
 if [[ $yn = [yY] ]]; then
-  curl -LO https://raw.githubusercontent.com/knterada5/.dotfiles/main/install_linux.sh; source install_linux.sh
+  curl -LO https://raw.githubusercontent.com/matsurix28/dotfiles/main/install_linux.sh; source install_linux.sh
 else
   printf '\n'
 fi
